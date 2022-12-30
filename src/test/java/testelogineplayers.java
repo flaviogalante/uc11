ad54e85fc50107daa0bf24cab39e068fe9ddc73c
Flávio Machado Galante <109998820+flaviogalante@users.noreply.github.com>
2022-12-30 08:26:01 -0300

    Update testelogineplayers.java

---
+++ src/test/java/testelogineplayers.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteLoginEplayers {
+	private WebDriver driver;
+	
+	@Before
+	public void ConfigurarTeste()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://localhost:4200");
+	}
+	
+	@Test
+	public void TestarLogin()
+	{
+		driver.findElement(By.id("input-email")).sendKeys("aluno@senai.com");
+		driver.findElement(By.id("input-senha")).sendKeys("aluno");
+		driver.findElement(By.id("botao-enviar")).click();
+	}
+}
